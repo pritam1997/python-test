e130f668aad68369d5c360f61aff60e56c48e4cf
pritam1997 <46590926+pritam1997@users.noreply.github.com>
2019-02-22 12:58:39 +0530

    Add files via upload

--- q3.py
+++ q3.py
@@ -1,9 +1,3 @@
-n = int ( input ("HOW MUCH NO. YOU WNT TO ENTER. : "))
-l=[]
-for x in range(1,n+1):
-	i = int ( input ("Enter no. : "))
-	s=f'{i}'
-	l.append(s)
-print(l)
-t=tuple(l)
-print(t)
+n=input("Enter list of no. : ").split(",")
+print(n)
+print(set(n))
