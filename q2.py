bce55b1936477f552f602b9aaf1b6001f1c594e2
pritam1997 <46590926+pritam1997@users.noreply.github.com>
2019-02-22 12:54:54 +0530

    Add files via upload

--- q2.py
+++ q2.py
@@ -1,12 +1,7 @@
-# i=int(input("Enter no. : "))
-i=4
-x=i/2
+n = int(input("Enter no. : "))
 l=[]
-for n in range(1,i+1):
-	a=n
-	if a<=x:	
-		print(a)
-		l.append([[]])
-	n=n*n
+for i in range(1,n+1):
+	l.append(i)
 
-print(l)
+d=dict((i,i*i) for i in l)
+print(d)
